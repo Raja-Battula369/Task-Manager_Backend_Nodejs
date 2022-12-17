0d4382df59fb5242f7a30d4ae046d3c4a4c3d5ea
Raja-Battula369 <battulavenkatamuralikrishnaraj@gmail.com>
2022-12-17 14:10:23 +0530

    second commit

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const mongoose=require('mongoose');
 const dotenv=require('dotenv');
 dotenv.config({path:'./config.env'});
@@ -8,10 +7,11 @@ const db=process.env.DATABASE.replace('<password>',process.env.DATABASE_PASSWORD
 mongoose.connect(db,{
     useNewUrlParser:true,
     useUnifiedTopology:true
-}).then(()=>console.log('Successfully COnneted'));
+}).then(()=>console.log('Successfully Conneted to DataBase!!!!!!!!!!!'));
+
 
 const app=require('./app');
 
 app.listen(3000,()=>{
-    console.log('ON');
+    console.log('server listening!!!!!!');
 })
